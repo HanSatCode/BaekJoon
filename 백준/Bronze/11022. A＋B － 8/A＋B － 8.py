ed033f47b71de1c3eaacbc34be1b1fbe3350ced2
Han028 <hansatcode@kakao.com>
2022-07-17 00:30:36 +0900

    [Bronze V] Title: A+B - 8, Time: 72 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,15 @@
+count = int(input())
+
+x = 0
+a = 0
+b = 0
+memory = list()
+
+for x in range(0, count) :
+    a, b = input().split()
+    memory.append([int(a),int(b)])
+
+x = 0
+
+for x in range(0, count) :
+    print("Case #%d: %d + %d = %d" % (x+1, memory[x][0], memory[x][1], memory[x][0]+memory[x][1]))

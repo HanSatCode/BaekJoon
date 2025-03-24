3270ffbbbb030f14fe16b2b9e4d3928fa76d8b24
Han028 <seki02@naver.com>
2025-03-24 20:23:55 +0900

    [Bronze V] Title: Serca, Time: 176 ms, Memory: 110896 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,7 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+for x in range(n):
+    print(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @")
+    print(" @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     ")

967ce0a4b3a705ce408d748751cda6059dcec108
Han028 <seki02@naver.com>
2025-12-27 22:04:47 +0900

    [Bronze IV] Title: 연대 다음 고대, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34824. 연대 다음 고대/연대 다음 고대.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for x in range(n):
+    name = input()
+    if name == "yonsei":
+        print("Yonsei Won!")
+        break
+    if name == "korea":
+        print("Yonsei Lost...")
+        break

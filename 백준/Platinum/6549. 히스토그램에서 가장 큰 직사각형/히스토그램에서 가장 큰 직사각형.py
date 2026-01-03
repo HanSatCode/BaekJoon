006f3af94b798dca42df9e278c4487abe0c5606c
Han028 <seki02@naver.com>
2026-01-03 19:55:20 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 1440 ms, Memory: 66072 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -1,4 +1,5 @@
 import sys
+sys.setrecursionlimit(10**6)
 
 while True:
     inputArray = list(map(int, sys.stdin.readline().strip().split())) # 처음은 크기, 나머지는 높이
@@ -8,22 +9,20 @@ while True:
     size = 1 # 세그먼트 트리 크기
     while size < N: size *= 2 # 세그먼트 트리 크기 계산
 
-    inputArray.append(10**9+1) # 인덱스 1부터 시작, 마지막에 큰 값 추가
+    inputArray = inputArray + [10**9 + 1] # 인덱스 1부터 시작, 마지막에 큰 값 추가
     lastIdx = N + 1 # 마지막 인덱스 (큰 값 위치)
-    tree = [lastIdx] * (size * 2) # 세그먼트 트리 크기 할당
+    tree = [0] * (size * 2) # 세그먼트 트리 크기 할당
 
-    for i in range(1, N + 1): # 리프 노드에 인덱스 저장
-        tree[size + i - 1] = i
-
-    for x in range(size - 1, 0, -1): # 리프 노드 외의 노드 채우기
-        leftChild = tree[x * 2]
-        rightChild = tree[x * 2 + 1]
-
-        if leftChild == 0: tree[x] = rightChild # 왼쪽 자식이 비어있으면 오른쪽 자식으로
-        elif rightChild == 0: tree[x] = leftChild # 오른쪽 자식이 비어있으면 왼쪽 자식으로
-        else:
-            if inputArray[leftChild] < inputArray[rightChild]: tree[x] = leftChild
-            else: tree[x] = rightChild
+    def recursive(node, start, end): # 세그먼트 트리 생성 (베이스 인덱스 1)
+        if start == end: # 리프 노드일 경우?
+            tree[node] = start if start <= N else lastIdx
+            return tree[node]
+        mid = (start + end) // 2 # 중간
+        leftChild = recursive(node * 2, start, mid)
+        rightChild = recursive(node * 2 + 1, mid + 1, end)
+        if(inputArray[leftChild] < inputArray[rightChild]): tree[node] = leftChild # 람다 -> if-else로 대체하기
+        else: tree[node] = rightChild
+        return tree[node]
 
     def query(left, right): # 포인터임. 왼쪽 자식, 오른쪽 자식 아님!
         shortestLengthIndex = lastIdx
@@ -42,17 +41,14 @@ while True:
             left //= 2 # 굳이 형제 노드를 보지 않는 이유? = 부모에 이미 내 형제와 비교한 최솟값이 저장되어 있기 때문
             right //= 2
         return shortestLengthIndex
-    
-    maxArea = 0
-    stack = [(1, N)] # 탐색 구간 스택
-    while stack:
-        start, end = stack.pop()
-        if start > end: continue
-        minIndex = query(start, end)
-        area = (end - start + 1) * inputArray[minIndex]
-        if area > maxArea:
-            maxArea = area
-        stack.append((start, minIndex - 1))
-        stack.append((minIndex + 1, end))
 
-    print(maxArea) # 재귀를 아예 안쓰는 방향으로...
+    def solve(start, end): # 최대 직사각형 넓이 계산
+        if start > end: return 0
+        index = query(start, end) # 구간 최솟값 인덱스
+        area = inputArray[index] * (end - start + 1) # 현 구간 전체를 덮는 직사각형 넓이
+        if start <= index - 1: area = max(area, solve(start, index - 1)) # 왼쪽 구간을 구할 수 있으면?
+        if index + 1 <= end: area = max(area, solve(index + 1, end)) # 오른쪽 구간을 구할 수 있으면?
+        return area
+
+    recursive(1, 1, size) # 세그먼트 트리 생성 (베이스 인덱스 1)
+    print(solve(1, N))

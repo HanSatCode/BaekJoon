f6753abe143a0345dc38cc9b1f59068ae8742f2b
Han028 <seki02@naver.com>
2026-02-16 20:02:57 +0900

    [Gold V] Title: 회문, Time: 212 ms, Memory: 183784 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -1,5 +1,11 @@
 import sys
 
+def judge(left, right, T):
+    while left < right:
+        if T[left] != T[right]: return False
+        else: left += 1; right -= 1
+    return True
+
 N = int(sys.stdin.readline().strip())
 for _ in range(N):
     T = list(sys.stdin.readline().strip())
@@ -7,9 +13,8 @@ for _ in range(N):
 
     while leftPointer < rightPointer:
         if T[leftPointer] != T[rightPointer]:
-            LM = T[:leftPointer] + T[leftPointer + 1:] # 왼쪽을 바꿔보자
-            RM = T[:rightPointer] + T[rightPointer + 1:] # 오른쪽을 바꿔보자
-            if LM == LM[::-1] or RM == RM[::-1]: print("1"); break
+            # 앞에서 검사한건 이미 회문이라고 판단했으므로, 안에만 검사함.
+            if judge(leftPointer + 1, rightPointer, T) or judge(leftPointer, rightPointer - 1, T): print("1"); break
             else: print("2"); break
         else: leftPointer += 1; rightPointer -= 1
     else: print("0") # 회문
